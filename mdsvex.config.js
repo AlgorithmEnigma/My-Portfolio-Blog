80bd7fb60816bad16d60b429a0fb6c56a33aa0d8
Jordan Lowell <wraithbournejl@gmail.com>
2022-08-11 18:31:05 -0400

    Reworking file structure

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,13 +1,10 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
 // import { join } from 'path';
 
-const path_to_layout = './src/lib/components/Layout.svelte';
-
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
 
 	smartypants: true,
-	layout: path_to_layout,
 	remarkPlugins: [],
 	rehypePlugins: []
 });
